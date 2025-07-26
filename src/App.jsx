1ea6a9747d96003faff4b5a449b835f3f542caf2
Developer-Linus <linusmksu30@gmail.com>
2025-07-26 11:55:05 +0300

    Completed the setup for the Mojito website.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import React from 'react'
+import gsap from 'gsap';
+
+// Register Gsap animation plugins globally in the app
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+
+const App = () => {
+  return (
+    <div className='flex-center'>
+      <h1 className='text-3xl text-indigo-300'>Hello, GSAP</h1>
+    </div>
+  )
+}
+
+export default App
